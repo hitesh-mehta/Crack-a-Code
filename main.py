6e625f855492a64ee8c802491a8d3f3a642db142
Hitesh Mehta <hteshpooja@gmail.com>
2025-05-24 21:39:21 +0530

    host changed

--- main.py
+++ main.py
@@ -344,7 +344,7 @@ if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(
         "main:app",
-        host="127.0.0.0",
+        host="0.0.0.0",
         port=port,
         reload=False,  # Set to False for production
         log_level="info"
